24dce1eef263c838fabd7d1b729a8061b2b47105
Steve Waldman <swaldman@mchange.com>
2016-05-05 18:51:51 -0700

    Generalize toSetIfNoDups / toMapIfNoDupKey methods from Seq to Iterables

--- src/main/scala/com/mchange/sc/v2/collection/package.scala
+++ src/main/scala/com/mchange/sc/v2/collection/package.scala
@@ -13,8 +13,8 @@ package object collection {
     * Returns as Right the successfully constructed set if there were no duplicates,
     * as Left the first duplicate value encountered if there were duplicates
     */ 
-  def seqToSetIfNoDups[T]( seq : Seq[T] ) : Either[T,scimmutable.Set[T]] = {
-    seq.foldLeft(Right(scimmutable.Set.empty[T]) : Either[T,scimmutable.Set[T]] ) { (accum, next) =>
+  def toSetIfNoDups[T]( iterable : Iterable[T] ) : Either[T,scimmutable.Set[T]] = {
+    iterable.foldLeft(Right(scimmutable.Set.empty[T]) : Either[T,scimmutable.Set[T]] ) { (accum, next) =>
       accum.flatMap( set => if (set.contains(next)) Left(next) else Right(set + next) )
     }
   }
@@ -23,8 +23,8 @@ package object collection {
     * Returns as Right the successfully constructed Map if there were no duplicate keys,
     * as Left the first duplicate key encounted if there were duplicate keys
     */ 
-  def seqToMapIfNoDupKeys[K,V]( seq : Seq[(K,V)] ) : Either[K,scimmutable.Map[K,V]] = {
-    seq.foldLeft(Right(scimmutable.Map.empty[K,V]) : Either[K,scimmutable.Map[K,V]]) { (accum, next) =>
+  def toMapIfNoDupKeys[K,V]( iterable : Iterable[(K,V)] ) : Either[K,scimmutable.Map[K,V]] = {
+    iterable.foldLeft(Right(scimmutable.Map.empty[K,V]) : Either[K,scimmutable.Map[K,V]]) { (accum, next) =>
       accum.flatMap( m => if (m.contains(next._1)) Left(next._1) else Right(m + next) )
     }
   }
@@ -33,8 +33,8 @@ package object collection {
     * Returns as Right the successfully constructed SortedMap if there were no duplicate keys,
     * as Left the first duplicate key encounted if there were duplicate keys
     */ 
-  def seqToSortedMapIfNoDupKeys[K,V]( seq : Seq[(K,V)] )( implicit ordering : Ordering[K] ) : Either[K,scimmutable.SortedMap[K,V]] = {
-    seq.foldLeft(Right(scimmutable.SortedMap.empty[K,V]) : Either[K,scimmutable.SortedMap[K,V]]) { (accum, next) =>
+  def toSortedMapIfNoDupKeys[K,V]( iterable : Iterable[(K,V)] )( implicit ordering : Ordering[K] ) : Either[K,scimmutable.SortedMap[K,V]] = {
+    iterable.foldLeft(Right(scimmutable.SortedMap.empty[K,V]) : Either[K,scimmutable.SortedMap[K,V]]) { (accum, next) =>
       accum.flatMap( m => if (m.contains(next._1)) Left(next._1) else Right(m + next) )
     }
   }
