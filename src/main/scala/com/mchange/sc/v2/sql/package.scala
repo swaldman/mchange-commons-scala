9bb433b025b30ceb2cb4dbb6d39481c8a08d020c
Steve Waldman <swaldman@mchange.com>
2016-07-30 20:13:17 -0700

    Add getMaybeSingleValue( rs : ResultSet ) and related convenience methods

--- src/main/scala/com/mchange/sc/v2/sql/package.scala
+++ src/main/scala/com/mchange/sc/v2/sql/package.scala
@@ -3,6 +3,7 @@ package com.mchange.sc.v2
 import java.sql.ResultSet
 
 package object sql {
+
   final class UnexpectedRowCountException( message : String ) extends Exception( message )
 
   def getSingleValue[T]( extractor : ResultSet => T)( rs : ResultSet ) : T = {
@@ -18,4 +19,22 @@ package object sql {
   def getSingleLong( rs : ResultSet )    = getSingleValue( _.getLong(1) )( rs )
   def getSingleFloat( rs : ResultSet )   = getSingleValue( _.getFloat(1) )( rs )
   def getSingleDouble( rs : ResultSet )  = getSingleValue( _.getDouble(1) )( rs )
+
+  def getMaybeSingleValue[T]( extractor : ResultSet => T)( rs : ResultSet ) : Option[T] = {
+    if (! rs.next() ) {
+      None
+    } else {
+      val out = extractor( rs )
+      if ( rs.next() ) throw new UnexpectedRowCountException("A result set expect to contain precisely one row contained more than one row")
+      Some( out )
+    }
+  }
+
+  def getMaybeSingleString( rs : ResultSet )  = getMaybeSingleValue( _.getString(1) )( rs )
+  def getMaybeSingleBoolean( rs : ResultSet ) = getMaybeSingleValue( _.getBoolean(1) )( rs )
+  def getMaybeSingleInt( rs : ResultSet )     = getMaybeSingleValue( _.getInt(1) )( rs )
+  def getMaybeSingleLong( rs : ResultSet )    = getMaybeSingleValue( _.getLong(1) )( rs )
+  def getMaybeSingleFloat( rs : ResultSet )   = getMaybeSingleValue( _.getFloat(1) )( rs )
+  def getMaybeSingleDouble( rs : ResultSet )  = getMaybeSingleValue( _.getDouble(1) )( rs )
+
 }
