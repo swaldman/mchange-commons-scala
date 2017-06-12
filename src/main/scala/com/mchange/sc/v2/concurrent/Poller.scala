5cc6111a119de39aee396ba2c225598291001b5b
Steve Waldman <swaldman@mchange.com>
2017-06-11 23:30:31 -0700

    Reorganize Poller into trait and separate implementations

--- src/main/scala/com/mchange/sc/v2/concurrent/Poller.scala
+++ src/main/scala/com/mchange/sc/v2/concurrent/Poller.scala
@@ -1,62 +1,56 @@
 package com.mchange.sc.v2.concurrent
 
-import java.util.concurrent.{ScheduledThreadPoolExecutor,ScheduledFuture,TimeUnit}
+import java.util.concurrent.ScheduledExecutorService
 
-import scala.concurrent.{Future,Promise}
+import scala.concurrent.Future
 import scala.concurrent.duration._
-import scala.util.Try
-import scala.util.control.NonFatal
 
 object Poller {
+  class PollerException( message : String, cause : Throwable ) extends Exception( message, cause )
   final class TimeoutException( label : String, deadline : Long ) extends Exception( s"Poller.Task '${label}' expired at ${new java.util.Date(deadline)}" )
+  final class ClosedException( instance : Poller ) extends Exception( s"Poller '${instance}' has been closed." )
 
-  /*
-   *  we separate task from Task.withDeadline so that user tasks are immutable and can be reusable
-   */ 
+  def withExternalExecutor( ses : ScheduledExecutorService ) : Poller = new ScheduledExecutorServicePoller.withExternalExecutor( ses )
+
+  def withInternalExecutor( corePoolSize : Int = 3 ) : Poller = new ScheduledExecutorServicePoller.withInternalExecutor( corePoolSize )
+
   object Task {
-    private [Poller] final case class withDeadline[T]( task : Task[T], deadline : Long )
+  /**
+    *  We separate task from Task.withDeadline[T] so that user tasks are immutable and can be reusable.
+    * 
+    *  Task.wthDeadline[T] useful to implementations, not clients or users of a Poller.
+    */ 
+    object withDeadline {
+      def apply[T]( task : Task[T] ) : Task.withDeadline[T] = {
+        val deadline = if ( task.timeout == Duration.Inf ) -1 else System.currentTimeMillis + task.timeout.toMillis
+        this.apply( task, deadline )
+      }
+    }
+    /**
+      * @param deadline, as Long in milliseconds since UNIX epoch, negative values mean no deadline!
+      */ 
+    final case class withDeadline[T] ( task : Task[T], deadline : Long ) {
+      def timedOut = deadline >= 0 && System.currentTimeMillis > deadline
+    }
   }
+
   class Task[T]( val label : String, val period : Duration, val pollFor : () => Boolean, val onSuccess : () => T, val timeout : Duration = Duration.Inf )
 }
-class Poller( stpe : ScheduledThreadPoolExecutor ) {
+trait Poller extends AutoCloseable {
+  def addTask[T]( task : Poller.Task[T] ) : Future[T]
 
-  def addTask[T]( task : Poller.Task[T] ) : Future[T] = {
-    val deadline = if ( task.timeout == Duration.Inf ) -1 else System.currentTimeMillis + task.timeout.toMillis
-
-    val promise = Promise[T]()
-    scheduleTask( Poller.Task.withDeadline( task, deadline ), promise )
-    promise.future
-  }
-
-  private def scheduleTask[T]( twd : Poller.Task.withDeadline[T], promise : Promise[T] ) : Unit = {
-
-    val task     = twd.task
-    val deadline = twd.deadline
-
-    val runnable = new Runnable {
-
-      private def timedOut = deadline >= 0 && System.currentTimeMillis > deadline
-
-      def run() : Unit = {
-        try {
-          if ( ! timedOut ) {
-            if ( task.pollFor() ) {
-              promise.complete( Try( task.onSuccess() ) )
-            } else {
-              Poller.this.scheduleTask( twd, promise )
-            }
-          } else {
-            promise.failure( new Poller.TimeoutException( task.label, deadline ) )
-          }
-        }
-        catch {
-          case NonFatal( unexpected ) => promise.failure( unexpected )
-        }
-      }
-
-    }
-
-    val millis = task.period.toMillis
-    stpe.schedule( runnable, millis, TimeUnit.MILLISECONDS )
-  }
+  /**
+    * Requests the poller stop polling (signalling failures if necessary on outstanding polls)
+    * and clean up any resources it  may have opened.
+    * 
+    * There is no guarantee that a poll will not succeed after a Poller has been shutdown,
+    * although "pretty soon" (in practice likely to mean after `task.period`), polls should begin
+    * to fail.
+    * 
+    * Whether any underlying Threads used to implement the poller are stopped is implementation dependent.
+    * Implementations that internally spawn their own Threads should shut them down. Those that make use of
+    * external Thread pools or utilities should leave those external utilities active until their creators
+    * shut them down.
+    */ 
+  def close() : Unit
 }
---
+++ src/main/scala/com/mchange/sc/v2/concurrent/ScheduledExecutorServicePoller.scala
@@ -0,0 +1,78 @@
+package com.mchange.sc.v2.concurrent
+
+import java.util.concurrent.{ScheduledExecutorService,ScheduledThreadPoolExecutor,ScheduledFuture,TimeUnit}
+
+import scala.concurrent.{Future,Promise}
+import scala.concurrent.duration._
+import scala.util.Try
+import scala.util.control.NonFatal
+
+
+object ScheduledExecutorServicePoller {
+  abstract class Abstract( protected val ses : ScheduledExecutorService ) extends Poller {
+    // MT: protected by this' lock
+    var closed = false
+
+    def isClosed : Boolean = this.synchronized { closed }
+
+    def addTask[T]( task : Poller.Task[T] ) : Future[T] = {
+
+      val promise = Promise[T]()
+      scheduleTask( Poller.Task.withDeadline( task ), promise )
+      promise.future
+    }
+
+    private def scheduleTask[T]( twd : Poller.Task.withDeadline[T], promise : Promise[T] ) : Unit = {
+      if ( isClosed ) {
+        promise.failure( new Poller.ClosedException( this ) )
+      } else {
+        val task     = twd.task
+        val deadline = twd.deadline
+
+        val runnable = new Runnable {
+
+          def run() : Unit = {
+            try {
+              if ( ! twd.timedOut ) {
+                if ( task.pollFor() ) {
+                  promise.complete( Try( task.onSuccess() ) )
+                } else {
+                  Abstract.this.scheduleTask( twd, promise )
+                }
+              } else {
+                promise.failure( new Poller.TimeoutException( task.label, deadline ) )
+              }
+            }
+            catch {
+              case NonFatal( unexpected ) => promise.failure( unexpected )
+            }
+          }
+
+        }
+
+        val millis = task.period.toMillis
+        ses.schedule( runnable, millis, TimeUnit.MILLISECONDS )
+      }
+    }
+
+    /**
+      * Since this implementation accepts an externally constructed ScheduledExecutorService, 
+      * we don't shut it down, just prevent new tasks from getting scheduled.
+      */ 
+    def close() : Unit = this.synchronized {
+      closed = true
+    }
+  }
+  final class withExternalExecutor( ses : ScheduledExecutorService ) extends ScheduledExecutorServicePoller.Abstract( ses )
+
+  final class withInternalExecutor( corePoolSize : Int = 3 ) extends ScheduledExecutorServicePoller.Abstract( new ScheduledThreadPoolExecutor( corePoolSize ) ) {
+    /**
+      * Here we construct and cleanup our own ScheduledThreadPoolExecutor
+      */ 
+    override def close() : Unit = {
+      super.close()
+      ses.shutdown() // gracefully shutdown the threadpool after existing tasks have executed
+    }
+  }
+}
+
