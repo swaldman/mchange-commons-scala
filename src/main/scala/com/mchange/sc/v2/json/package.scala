0f2f48c3188e60e7ec6e881c04c59805551e6569
Steve Waldman <swaldman@mchange.com>
2017-11-01 11:29:43 -0700

    Add Seq[Byte] forms to json cleanup functions

--- src/main/scala/com/mchange/sc/v2/json/package.scala
+++ src/main/scala/com/mchange/sc/v2/json/package.scala
@@ -1,15 +1,19 @@
 package com.mchange.sc.v2
 
 import scala.io.Codec
+import scala.collection._
 
 import java.nio.ByteBuffer
+
+import com.mchange.sc.v2.collection.immutable.ImmutableArraySeq
 
 package object json {
   case class Segregated( clean : String, controlCharacters : String ) {
     def escapedControlCharacterList : List[String] = controlCharacters.map( c => f"\\u${c}%04x" ).toList
     def escapedControlCharacters    : String       = escapedControlCharacterList.foldLeft("")(_ + _)
 
-    def cleanBytes( codec : Codec = Codec.UTF8 ) : Array[Byte] = clean.getBytes( codec.charSet )
+    def cleanByteArray( codec : Codec = Codec.UTF8 ) : Array[Byte]         = clean.getBytes( codec.charSet )
+    def cleanBytes( codec : Codec = Codec.UTF8 )     : immutable.Seq[Byte] = ImmutableArraySeq.Byte.createNoCopy( cleanByteArray( codec ) )
   }
 
   def segregateControlCharacters( input : String ) : Segregated = {
@@ -23,6 +27,14 @@ package object json {
   }
 
   def segregateControlCharacters( input : Array[Byte] ) : Segregated = {
+    segregateControlCharacters( input, Codec.UTF8 )
+  }
+
+  def segregateControlCharacters( input : Seq[Byte], codec : Codec ) : Segregated = {
+    segregateControlCharacters( input.toArray, codec )
+  }
+
+  def segregateControlCharacters( input : Seq[Byte] ) : Segregated = {
     segregateControlCharacters( input, Codec.UTF8 )
   }
 
@@ -42,7 +54,7 @@ package object json {
     }
   }
 
-  def removeNulTermination( input : Array[Byte], codec : Codec = Codec.UTF8 ) : Array[Byte] = {
+  def removeNulTermination( input : Array[Byte], codec : Codec ) : Array[Byte] = {
     codec match {
       case Codec.UTF8 | Codec.ISO8859 => {
         val len = input.length
@@ -60,6 +72,16 @@ package object json {
         removeNulTermination( new String( input, 0, input.length, cs ) ).getBytes( cs )
       }
     }
+  }
+
+  def removeNulTermination( input : Array[Byte] ) : Array[Byte] = removeNulTermination( input, Codec.UTF8 )
+
+  def removeNulTermination( input : Seq[Byte], codec : Codec ) : immutable.Seq[Byte] = {
+    ImmutableArraySeq.Byte.createNoCopy( removeNulTermination( input.toArray, codec ) )
+  }
+
+  def removeNulTermination( input : Seq[Byte] ) : immutable.Seq[Byte] = {
+    removeNulTermination( input, Codec.UTF8 )
   }
 
   private def notNulTerminated( input : String ) : Boolean = {
